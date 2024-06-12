23824e17228c73a924b626e80f6e44430c314668
javici <javicasados@gmail.com>
2024-06-12 10:47:55 +0200

    Initial commit

---
+++ apps/frontend-training-zone/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'frontend-training-zone',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/frontend-training-zone',
+};
