d5ef26003f1d70c0bed8ffe6f66ea57967c7d350
javihertz <javicasados@gmail.com>
2024-06-17 10:54:17 +0200

    refactor(vite): cleanup config

--- apps/frontend-training-zone/vite.config.ts
+++ apps/frontend-training-zone/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: __dirname,
@@ -16,14 +16,7 @@ export default defineConfig({
     port: 4300,
     host: 'localhost',
   },
-
   plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
   build: {
     outDir: '../../dist/apps/frontend-training-zone',
     emptyOutDir: true,
